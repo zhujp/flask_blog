4482e2f084615a453ac90c5ff574cbfa49445b08
vilay <2546166552@qq.com>
2018-06-21 23:51:24 +0800

    前台首页开发

--- app/frontend/views.py
+++ app/frontend/views.py
@@ -1,10 +1,21 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import frontend
-from ..models import Category, Label, Post, Link
+from ..models import Category, Label, Post, Link,Setting
 
 @frontend.route('/')
 def index():
-    return render_template('index.html');
+    nav = Category.query.filter_by(enabled=True).all()
+    page = request.args.get('page',1, type=int)
+    pagination = Post.query.filter_by(enabled=True).order_by(Post.created_at.desc()).paginate(page,per_page=15,error_out=False)
+    posts = pagination.items
+    for post in posts:
+        post.body = post.body[0:250]
+
+    sets = Setting.query.all()
+    site = {}
+    for s in sets:
+        site[s.name] = s.value
+    return render_template('index.html',nav=nav,posts=posts,site=site,pagination=pagination)
 
 
 @frontend.route('/category/<int:id>')
